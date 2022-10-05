dc2a611a3017154f808ebcc56bef35d080a1c778
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2022-10-05 23:15:10 +0100

    Initial Commit

---
+++ SoBored/SoBoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoBoredApp.swift
+//  SoBored
+//
+//  Created by Yash Shah on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoBoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
